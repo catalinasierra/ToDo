0a9a89082f0c170a2819def3e01188a51c2f8efe
LAURA MALAGON <laura.malagon@mail.escuelaing.edu.co>
2022-11-18 18:38:13 -0500

    Clase para una tarea

---
+++ js/todo.js
@@ -0,0 +1,8 @@
+class ToDo {
+    constructor(id, titulo, fecha, estatus){
+        this.id = id;
+        this.titulo= titulo;
+        this.fecha = fecha;
+        this.estatus = estatus;
+    }
+}
